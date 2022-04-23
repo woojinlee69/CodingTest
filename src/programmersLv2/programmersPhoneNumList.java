3615f9175f4d7b1a3a9e75b568b4c90569ccd14e
woojinlee69 <33486207+woojinlee69@users.noreply.github.com>
2022-04-23 14:55:31 +0900

    Lv2

---
+++ src/programmersLv2/programmersPhoneNumList.java
@@ -0,0 +1,26 @@
+package programmersLv2;
+
+import java.util.Arrays;
+//score : 75 / effi. Test : 0
+// i와k 가 다르고 String startsWith 가 True 인것
+
+public class programmersPhoneNumList {
+    public static boolean solution(String[] phone_book){
+        boolean answer = true;
+
+        for(int i = 0 ; i < phone_book.length ; i++) {
+            for (int k = 1; k < phone_book.length; k++) {
+                if (i!=k && phone_book[k].startsWith(phone_book[i]) == true) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+    public static void main(String[] args){
+        String[] tmp = {"119", "97674", "11955243"};
+        solution(tmp);
+    }
+}
--- src/programmersLv2/programmersPocketmon.java
+++ src/programmersLv2/programmersPocketmon.java
@@ -2,6 +2,7 @@ package programmersLv2;
 
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class programmersPocketmon {
@@ -10,18 +11,24 @@ public class programmersPocketmon {
         int ea = nums.length/2;
 
         PriorityQueue pq = new PriorityQueue<>();
+        HashSet<Integer> hash = new HashSet<>();
 
         for(int i = 0 ; i < nums.length ; i ++)
         {
-            pq.add(nums[i]);
+            hash.add(nums[i]);
         }
-        System.out.println(pq.poll());
-        System.out.println(pq.poll());
-        System.out.println(pq.poll());
+        System.out.println(hash);
+        int tmp = hash.size();
+
+        if( ea > tmp ){
+            answer = tmp;
+        }else
+            answer = ea;
+        System.out.println(answer);
         return answer;
     }
     public static void main(String[] args){
-        int[] arr = {3,1,2,3};
+        int[] arr = {3,3,3,2,2,4};
         solution(arr);
     }
 }
